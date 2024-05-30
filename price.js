888360608b757d142b5f1a1c31a08b017116f8e6
foffpls <71446302+foffpls@users.noreply.github.com>
2024-05-30 15:19:58 +0300

    Add files via upload

--- price.js
+++ price.js
@@ -68,6 +68,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var weightRadio = document.getElementById("weightRadio");
     var packagedRadio = document.getElementById("packagedRadio");
     var allRadio = document.getElementById("allRadio");
+	var warehouseVIPRadio = document.getElementById("warehouseVIPRadio");
 
     searchInput.addEventListener("input", filterItems);
     warehouse7Radio.addEventListener("change", filterItems);
@@ -75,6 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
     weightRadio.addEventListener("change", filterItems);
     packagedRadio.addEventListener("change", filterItems);
     allRadio.addEventListener("change", filterItems);
+	warehouseVIPRadio.addEventListener("change", filterItems);
 
     // Завантаження даних та фільтрація
     loadDataAndFilter();
@@ -85,9 +87,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function filterItems() {
 		var searchValue = searchInput.value.toLowerCase();
-		var filterValue = document.querySelector('input[type="radio"]:checked').value;
 		var showWarehouse7 = warehouse7Radio.checked;
 		var showWarehouseIncome = warehouseIncomeRadio.checked;
+		var showWarehouseVIP = warehouseVIPRadio.checked; // Додано
 		var showWeight = weightRadio.checked;
 		var showPackaged = packagedRadio.checked;
 		var showAll = allRadio.checked;
@@ -95,19 +97,17 @@ document.addEventListener("DOMContentLoaded", function () {
 		var rows = tableBody.getElementsByTagName("tr");
 		var displayedGroups = {};
 
-		// Показуємо всі рядки товарів та груп
 		for (var i = 0; i < rows.length; i++) {
-			rows[i].style.display = ""; // Показуємо всі рядки
+			rows[i].style.display = ""; 
 			var groupName = rows[i].getAttribute("data-group");
-			displayedGroups[groupName] = false; // Ініціалізуємо всі групи як невідображені
+			displayedGroups[groupName] = false; 
 		}
 
-		// Фільтруємо товари
 		for (var i = 0; i < rows.length; i++) {
 			var itemName = rows[i].getElementsByTagName("td")[0]?.innerText.toLowerCase();
 			var groupName = rows[i].getAttribute("data-group");
 
-			if (itemName && !rows[i].classList.contains("group-row")) { // Перевіряємо, чи це не рядок групи
+			if (itemName && !rows[i].classList.contains("group-row")) {
 				var matchesSearch = itemName.includes(searchValue);
 				var matchesFilter = (
 					showAll ||
@@ -118,152 +118,144 @@ document.addEventListener("DOMContentLoaded", function () {
 				var isInWarehouse7 = rows[i].classList.contains("warehouse-7");
 				var isInWarehouseIncome = rows[i].classList.contains("warehouse-income");
 
-				// Додаткова перевірка на відображення товару лише для надходжень
-				var matchesIncomeFilter = !showWarehouseIncome || isInWarehouseIncome;
-
 				var shouldDisplay = matchesSearch && matchesFilter &&
-					((showWarehouse7 && isInWarehouse7) ||
-					(showWarehouseIncome && isInWarehouseIncome));
+					(
+						(showWarehouse7 && isInWarehouse7 && !itemName.includes("акция вип")) ||
+						(showWarehouseIncome && isInWarehouseIncome && !itemName.includes("акция вип")) ||
+						(showWarehouseVIP && itemName.includes("акция вип"))
+					);
 
-				if (shouldDisplay && matchesIncomeFilter) { // Додано перевірку на відображення товару для надходжень
+				if (shouldDisplay) {
 					rows[i].style.display = "";
-					displayedGroups[groupName] = true; // Позначаємо групу як відображену
-
-					// Встановлюємо data-expanded="true" для рядків групи
-					if (rows[i].classList.contains("group-row")) {
-						rows[i].setAttribute("data-expanded", "true");
-					}
+					displayedGroups[groupName] = true; 
 				} else {
 					rows[i].style.display = "none";
 				}
 			}
 		}
 
-		// Приховуємо рядки груп, до яких не належить жоден видимий елемент
 		var groupRows = tableBody.getElementsByClassName("group-row");
 		for (var i = 0; i < groupRows.length; i++) {
 			var groupName = groupRows[i].getAttribute("data-group");
 			if (!displayedGroups[groupName]) {
 				groupRows[i].style.display = "none";
 			} else {
-				groupRows[i].style.display = ""; // Показуємо групи, які містять хоча б один видимий елемент
+				groupRows[i].style.display = "";
 			}
-			// Зберігаємо стан розгорнутості групи
 			if (groupRows[i].getAttribute("data-group") in groupStates) {
 				displayedGroups[groupName] = groupStates[groupName];
 			} else {
-				groupStates[groupName] = true; // Зберігаємо значення за замовчуванням, якщо група ще не існує
+				groupStates[groupName] = true;
 			}
 		}
 	}
 	
 	
     // Функція для завантаження XML даних
-    function loadXMLData(callback) { // Додаємо параметр callback
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", "Price.xml", true);
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var xmlDoc = xmlhttp.responseXML;
-                var goods = xmlDoc.getElementsByTagName("Goods");
-                var groupedItems = {};
+    function loadXMLData(callback) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "Price.xml", true);
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				var xmlDoc = xmlhttp.responseXML;
+				var goods = xmlDoc.getElementsByTagName("Goods");
+				var groupedItems = {};
 
-                for (var i = 0; i < goods.length; i++) {
-                    var item = goods[i];
-                    var groupName = item.getAttribute("Group");
+				for (var i = 0; i < goods.length; i++) {
+					var item = goods[i];
+					var groupName = item.getAttribute("Group");
 
-                    if (!groupedItems[groupName]) {
-                        groupedItems[groupName] = [];
-                    }
+					if (!groupedItems[groupName]) {
+						groupedItems[groupName] = [];
+					}
 
-                    groupedItems[groupName].push(item);
-                }
+					groupedItems[groupName].push(item);
+				}
 
-                var excludedGroups = new Set(["Побутові товари", "Архив", "Поддони"]);
+				var excludedGroups = new Set(["Побутові товари", "Архив", "Поддони"]);
 
-                for (var groupName in groupedItems) {
-                    if (excludedGroups.has(groupName)) {
-                        continue;
-                    }
+				for (var groupName in groupedItems) {
+					if (excludedGroups.has(groupName)) {
+						continue;
+					}
 
-                    var groupRow = document.createElement("tr");
-					
-                    groupRow.innerHTML = "<td colspan='3'><b>Група " + groupName + "</b></td>";
-                    groupRow.classList.add("group-row");
-                    groupRow.setAttribute("data-group", groupName); // Додаємо атрибут data-group
-					groupRow.setAttribute("data-expanded", "true"); // Додаємо цей атрибут
-                    tableBody.appendChild(groupRow);
+					var groupRow = document.createElement("tr");
+					groupRow.innerHTML = "<td colspan='3'><b>Група " + groupName + "</b></td>";
+					groupRow.classList.add("group-row");
+					groupRow.setAttribute("data-group", groupName);
+					groupRow.setAttribute("data-expanded", "true");
+					tableBody.appendChild(groupRow);
 
-                    groupRow.addEventListener("click", function () {
-                        toggleGroupItems(this);
-                    });
+					groupRow.addEventListener("click", function () {
+						toggleGroupItems(this);
+					});
 
-                    var items = groupedItems[groupName];
-                    for (var j = 0; j < items.length; j++) {
-                        var item = items[j];
-                        var itemName = item.getAttribute("Name");
-                        var itemPriceStr = item.getAttribute("Price");
-                        var itemPrice = parseFloat(itemPriceStr.replace(',', '.')) * 1.2;
-                        var formattedPrice = itemPrice.toFixed(2);
-                        var itemLeftover = item.getAttribute("Leftover");
+					var items = groupedItems[groupName];
+					for (var j = 0; j < items.length; j++) {
+						var item = items[j];
+						var itemName = item.getAttribute("Name");
+						var itemPriceStr = item.getAttribute("Price");
+						var itemPrice = parseFloat(itemPriceStr.replace(',', '.')) * 1.2;
+						var formattedPrice = itemPrice.toFixed(2);
+						var itemLeftover = item.getAttribute("Leftover");
 
-                        var itemRow = document.createElement("tr");
-                        itemRow.innerHTML = "<td>" + itemName + "</td><td>" + formattedPrice + "</td><td>" + itemLeftover + "</td>";
+						var itemRow = document.createElement("tr");
+						itemRow.innerHTML = "<td>" + itemName + "</td><td>" + formattedPrice + "</td><td>" + itemLeftover + "</td>";
 
-                        var parentTagName = item.parentNode.tagName;
-                        var parentOfParentTagName = item.parentNode.parentNode.tagName;
+						var parentTagName = item.parentNode.tagName;
+						var parentOfParentTagName = item.parentNode.parentNode.tagName;
 
-                        var skipRow = false;
+						var skipRow = false;
 
-                        if (parentOfParentTagName === "Income") {
-                            itemRow.classList.add("warehouse-income");
-                        } else if (parentOfParentTagName === "Leftovers") {
-                            var warehouseName = item.parentNode.getAttribute("Name");
-                            if (warehouseName === "Склад Акция") {
-                                skipRow = true;
-                            } else {
-                                itemRow.classList.add("warehouse-7");
-                            }
-                        }
+						if (parentOfParentTagName === "Income") {
+							itemRow.classList.add("warehouse-income");
+						} else if (parentOfParentTagName === "Leftovers") {
+							var warehouseName = item.parentNode.getAttribute("Name");
+							if (warehouseName === "Склад Акция") {
+								skipRow = true;
+							} else {
+								itemRow.classList.add("warehouse-7");
+							}
+						}
 
-                        if (skipRow) {
-                            continue;
-                        }
+						if (skipRow) {
+							continue;
+						}
 
-                        itemRow.classList.add("group-" + groupName.toLowerCase().replace(/\s+/g, "-").replace(/[()]/g, "") + "-item", "product-row");
-                        itemRow.setAttribute("data-group", groupName);
-                        itemRow.style.display = "none";
+						if (itemName.includes("АКЦИЯ ВИП")) {
+							itemRow.classList.add("warehouse-vip");
+						}
 
-                        tableBody.appendChild(itemRow);
-                    }
+						itemRow.classList.add("group-" + groupName.toLowerCase().replace(/\s+/g, "-").replace(/[()]/g, "") + "-item", "product-row");
+						itemRow.setAttribute("data-group", groupName);
+						itemRow.style.display = "none";
 
-                    // Показуємо рядок групи при завантаженні сторінки
-                    groupRow.style.display = ""; // Додаємо цей рядок
-                }
+						tableBody.appendChild(itemRow);
+					}
 
-                if (callback) { // Викликаємо callback, якщо він переданий
-                    callback();
-                }
-            }
-        };
-        xmlhttp.send();
-    }
+					groupRow.style.display = "";
+				}
+
+				if (callback) {
+					callback();
+				}
+			}
+		};
+		xmlhttp.send();
+	}
 
     function toggleGroupItems(groupRow) {
 		var groupClass = groupRow.innerText.trim().substring(6).trim().toLowerCase().replace(/\s+/g, "-").replace(/[()]/g, "");
 		var groupItems = document.getElementsByClassName("group-" + groupClass + "-item");
 
-		// Отримуємо поточний стан групи
 		var groupName = groupRow.getAttribute("data-group");
 		var isExpanded = groupStates[groupName];
 		if (isExpanded === undefined) {
-			isExpanded = true; // Встановлюємо значення за замовчуванням, якщо група ще не існує
+			isExpanded = true;
 		}
 
-		// Виводимо поточний стан групи в консоль
 		console.log("Group is expanded:", isExpanded);
 
-		// Змінюємо стан групи та відображення її елементів
 		var displayStyle = isExpanded ? "none" : "";
 
 		for (var j = 0; j < groupItems.length; j++) {
@@ -271,8 +263,10 @@ document.addEventListener("DOMContentLoaded", function () {
 			var groupName = item.getAttribute("data-group");
 			var isInWarehouse7 = item.classList.contains("warehouse-7");
 			var isInWarehouseIncome = item.classList.contains("warehouse-income");
+			var isInWarehouseVIP = item.classList.contains("warehouse-vip");
 			var showWarehouse7 = warehouse7Radio.checked;
 			var showWarehouseIncome = warehouseIncomeRadio.checked;
+			var showWarehouseVIP = warehouseVIPRadio.checked;
 			var showWeight = weightRadio.checked;
 			var showPackaged = packagedRadio.checked;
 			var showAll = allRadio.checked;
@@ -284,17 +278,16 @@ document.addEventListener("DOMContentLoaded", function () {
 			);
 
 			var shouldDisplay = matchesFilter &&
-				((showWarehouse7 && isInWarehouse7) ||
-				(showWarehouseIncome && isInWarehouseIncome));
+				(
+					(showWarehouse7 && isInWarehouse7 && !item.innerText.toLowerCase().includes("акция вип")) ||
+					(showWarehouseIncome && isInWarehouseIncome && !item.innerText.toLowerCase().includes("акция вип")) ||
+					(showWarehouseVIP && isInWarehouseVIP)
+				);
 
-			// Застосовуємо стиль відображення до кожного елементу товару
 			item.style.display = shouldDisplay ? displayStyle : "none";
 		}
 
-		// Оновлюємо стан групи
 		groupStates[groupName] = !isExpanded;
-
-		// Виводимо оновлений стан групи в консоль
 		console.log("Group is now expanded:", !isExpanded);
 	}
 });
